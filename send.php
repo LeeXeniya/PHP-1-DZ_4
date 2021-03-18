489aea51d593f7a4dbba31e9bf5f52103e3b09f6
LeeXeniya <leexenya@gmail.com>
2021-03-18 17:48:33 +0300

    Create send.php

---
+++ send.php
@@ -0,0 +1,12 @@
+<?php
+
+include __DIR__ . '/funcs.php';
+$str = $_POST['text'];
+
+if(!empty($str)) {
+    $comments = get_comment();
+    $comments[] = $str;
+    $com = implode(PHP_EOL, $comments);
+    file_put_contents(__DIR__ . '/data/text.txt', $com);
+    header('Location: http://localhost:8008/index.php');
+}
