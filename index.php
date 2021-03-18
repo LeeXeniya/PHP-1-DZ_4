4e8bdaa29862712762357e52f817b4f7adc05a04
LeeXeniya <leexenya@gmail.com>
2021-03-18 17:47:30 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="ru">
+<h1>Гостевая книга</h1>
+
+<?php
+include __DIR__ . '/funcs.php';
+
+$comments = get_comment();
+
+if(isset($comments)) {
+        $comment = implode('<br/>',$comments);
+        echo $comment;
+}
+?>
+
+<form method="post" action="/send.php">
+    Комментарий: <input type="text" name="text">
+    <button type="submit">Отправить</button>
+</form>
+</html>
